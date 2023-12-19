5fb57b8e10e568416422f05809c51c5023c7fe1a
Jean Pedro <jeanjesuspedrobook@gmail.com>
2023-12-19 14:58:42 -0300

    verificações

--- script.js
+++ script.js
@@ -7,9 +7,30 @@ botao.addEventListener('click', function() {
 
     let resultado = (valor * (1 + (taxa/100))**tempo).toFixed(2);
 
+    if (valor === '' || tempo === '' || taxa === '') {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+
+    if (isNaN(valor) || isNaN(tempo) || isNaN(taxa)) {
+        alert('Por favor, insira valores numéricos válidos.');
+        return;
+    }
+
+    if (taxa < 0 || taxa > 100) {
+        alert('A taxa deve estar entre 0 e 100.');
+        return;
+    }
+
     if (resultado != 0) {
         document.getElementById('resulte').innerHTML = resultado;
     } else {
-        document.getElementById('resulte').innerHTML = Inválido;
+        document.getElementById('resulte').innerHTML = 'Resultado inválido';
+    }
+
+    if (!isNaN(resultado)) {
+        document.getElementById('resulte').innerHTML = resultado;
+    } else {
+        document.getElementById('resulte').innerHTML = 'Resultado inválido';
     }
 })
